d308e445ce870a63df14281237d01e49a50b71d5
Carrie Lai <carrie_k_y_lai@hotmail.com>
2011-09-13 10:03:43 -0700

    User signup complete

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,8 @@ module SampleApp
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
   end
 end
